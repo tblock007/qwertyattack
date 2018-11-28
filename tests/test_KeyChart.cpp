a02b42fdf3b424c27ea1f18be4bd2912f3ac934e
tblock007 <the.t.block@gmail.com>
2018-11-27 17:25:23 -0700

    trying easy test

--- tests/test_KeyChart.cpp
+++ tests/test_KeyChart.cpp
@@ -1,20 +1,24 @@
-#include "catch.hpp"
 #include "../KeyChart.hpp"
+#include "catch.hpp"
 
 namespace qa {
 
-	TEST_CASE("Importing and extracting from KeyChart file #1", "[keychart]") {
-
-		KeyChart kut;
-		kut.importFile("testdata/test1.kc");
-
-		REQUIRE(kut.getSongFile() == "alphabet.ogg");
-		REQUIRE(kut.getTitle() == "Alphabet Song");
-		REQUIRE(kut.getArtist() == "Misc");
-		REQUIRE(kut.getGenre() == "Variety");
-
-		REQUIRE(!kut.getKeyNote(0).has_value());
-		REQUIRE(kut.getKeyNote(10000000).has_value());
-	}
-
+TEST_CASE("Importing and extracting from KeyChart file #1", "[keychart]")
+{
+   REQUIRE(1 == 1);
+   REQUIRE(18 == 18);
+   REQUIRE(2 + 3 == 5);
+   REQUIRE(0 == 1);
+   //		KeyChart kut;
+   //		kut.importFile("testdata/test1.kc");
+   //
+   //		REQUIRE(kut.getSongFile() == "alphabet.ogg");
+   //		REQUIRE(kut.getTitle() == "Alphabet Song");
+   //		REQUIRE(kut.getArtist() == "Misc");
+   //		REQUIRE(kut.getGenre() == "Variety");
+   //
+   //		REQUIRE(!kut.getKeyNote(0).has_value());
+   //		REQUIRE(kut.getKeyNote(10000000).has_value());
 }
+
+}  // namespace qa
