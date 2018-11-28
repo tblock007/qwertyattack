e1b6010400d12f2dcd5df03325c54c10764c522f
tblock007 <the.t.block@gmail.com>
2018-11-27 18:50:59 -0700

    testing CI

--- tests/test_KeyChart.cpp
+++ tests/test_KeyChart.cpp
@@ -5,17 +5,16 @@ namespace qa {
 
 TEST_CASE("Importing and extracting from KeyChart file #1", "[keychart]")
 {
-   REQUIRE(1 == 1);
-   //		KeyChart kut;
-   //		kut.importFile("testdata/test1.kc");
-   //
-   //		REQUIRE(kut.getSongFile() == "alphabet.ogg");
-   //		REQUIRE(kut.getTitle() == "Alphabet Song");
-   //		REQUIRE(kut.getArtist() == "Misc");
-   //		REQUIRE(kut.getGenre() == "Variety");
-   //
-   //		REQUIRE(!kut.getKeyNote(0).has_value());
-   //		REQUIRE(kut.getKeyNote(10000000).has_value());
+   		KeyChart kut;
+   		kut.importFile("./testdata/test1.kc");
+
+   		REQUIRE(kut.getSongFile() == "alphabet.ogg");
+   		REQUIRE(kut.getTitle() == "Alphabet Song");
+   		REQUIRE(kut.getArtist() == "Misc");
+   		REQUIRE(kut.getGenre() == "Variety");
+
+   		REQUIRE(!kut.getKeyNote(0).has_value());
+   		REQUIRE(kut.getKeyNote(10000000).has_value());
 }
 
 }  // namespace qa
